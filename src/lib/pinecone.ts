da471d9f68b8141f3b2d252a79f0838928141e79
Maria <sawaby@users.noreply.github.com>
2023-11-10 16:14:46 -0800

    embeddings, pinecone, langchain, pdf-parse, openai, hashing with type/md5

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,7 +1,10 @@
-import { Pinecone } from "@pinecone-database/pinecone";
+import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
 // read from filesystem and give back a text of pdf file
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { Document, RecursiveCharacterTextSplitter } from '@pinecone-database/doc-splitter';
+import { getEmbeddings } from "./embeddings";
+import md5 from "md5";
 
 export const getPineconeClient = () => {
   return new Pinecone({
@@ -9,7 +12,12 @@ export const getPineconeClient = () => {
     apiKey: process.env.PINECONE_API_KEY!,
   });
 }
-
+type PDFPage = {
+    pageContent: string;
+    metadata: {
+        loc: {pageNumber:number}
+    }
+}
 export async function loadS3IntoPinecone(fileKey: string){
     //1. obtain pdf -> download and read from pdf
     console.log("downloading s3 into file system.");
@@ -18,6 +26,55 @@ export async function loadS3IntoPinecone(fileKey: string){
         throw new Error("could not download from S3");
     }
     const loader = new PDFLoader(file_name);
-    const pages = await loader.load();
-    return pages;
+    const pages = (await loader.load()) as PDFPage[];
+    
+   
+    const document = await Promise.all(pages.map(prepareDocument))
+    
 }
+
+// vectorize and embed individual document
+async function embedDocument(doc: Document) {
+    try{
+        const embeddings = await getEmbeddings(doc.pageContent)
+        const hash = md5(doc.pageContent);
+// return the vector type coming from pinecone
+    return {
+        id: hash,
+        values: embeddings,
+        metadata: {
+        text: doc.metadata.text,
+        pageNumber: doc.metadata.pageNumber,
+        },
+    } as PineconeRecord;
+
+
+    
+    }catch(error){
+        console.log("error embedding document", error)
+        throw error
+    }
+}
+
+
+export const truncateStringByByte = ( str: string, bytes: number ) => {
+    const enc = new TextEncoder()
+    return new TextDecoder('utf-8').decode(enc.encode(str).slice(0, bytes))
+}
+ //2. split and segment the pdf
+ async function prepareDocument(page: PDFPage){
+    let {pageContent, metadata} = page
+    pageContent = pageContent.replace(/\n/g, '');
+    // split the code
+    const splitter = new RecursiveCharacterTextSplitter()
+    const docs = await splitter.splitDocuments([
+        new Document({
+            pageContent,
+            metadata: {
+                pageNumber: metadata.loc.pageNumber,
+                text: truncateStringByByte(pageContent, 36000)
+            }
+        })
+    ])
+    return docs
+ }
--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,39 +1,82 @@
+// import { S3 } from "@aws-sdk/client-s3";
+// import fs from "fs";
+
+// export async function downloadFromS3(file_key: string): Promise<string>{
+//     return new Promise(async (resolve, reject) => {
+//         console.log("here1")
+//         try {
+//         const s3 = new S3({
+//             region: 'us-west-1',
+//             credentials: {
+//                 accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+//                 secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!
+//             },
+            
+//         });
+//         console.log("here s3", s3)
+//         const params = {
+//             Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+//             Key: file_key,
+//         };
+
+//         const obj = await s3.getObject(params);
+        // console.log('obj', obj)
+        // download to local folder
+        // const file_name = `/tmp/maria${Date.now().toString()}.pdf`;
+        // if (obj.Body instanceof require("stream").Readable) {
+        // const file = fs.createWriteStream(file_name);
+        // file.on("open", function (fd) {
+        //     // @ts-ignore
+        //     obj.Body?.pipe(file).on("finish", () => {
+        //       return resolve(file_name);
+        //     });
+        //   });
+        // }
+//         } catch(error){
+//             console.log(error);
+//             reject(error);
+//             return null;
+//         }
+//     });
+// }
 import { S3 } from "@aws-sdk/client-s3";
 import fs from "fs";
+export async function downloadFromS3(file_key: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const s3 = new S3({
+        region: 'us-west-1',
+        credentials: {
+            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!
+        },
+      });
+      const params = {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+        Key: file_key,
+      };
+      console.log("params, ", params)
+      const obj = await s3.getObject(params);
+      console.log("obj: ", obj)
+      const file_name = `maria${Date.now().toString()}.pdf`;
 
-export async function downloadFromS3(file_key: string): Promise<string>{
-    return new Promise(async (resolve, reject) => {
-        try {
-        const s3 = new S3({
-            region: 'us-west-1',
-            credentials: {
-                accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
-                secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!
-            },
-            
-        });
-
-        const params = {
-            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
-            Key: file_key,
-        };
-
-        const obj = await s3.getObject(params);
-        // download to local folder
-        const file_name = `/tmp/elliott${Date.now().toString()}.pdf`;
-        if (obj.Body instanceof require("stream").Readable) {
+      if (obj.Body instanceof require("stream").Readable) {
+        // AWS-SDK v3 has some issues with their typescript definitions, but this works
+        // https://github.com/aws/aws-sdk-js-v3/issues/843
+        //open the writable stream and write the file
         const file = fs.createWriteStream(file_name);
         file.on("open", function (fd) {
-            // @ts-ignore
-            obj.Body?.pipe(file).on("finish", () => {
-              return resolve(file_name);
-            });
+          // @ts-ignore
+          obj.Body?.pipe(file).on("finish", () => {
+            return resolve(file_name);
           });
-        }
-        } catch(error){
-            console.log(error);
-            reject(error);
-            return null;
-        }
-    });
+        });
+        // obj.Body?.pipe(fs.createWriteStream(file_name));
+      }
+    } catch (error) {
+      console.error(error);
+      reject(error);
+      return null;
+    }
+  });
 }
